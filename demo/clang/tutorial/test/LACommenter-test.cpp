c5e14c73fe96f19db73c402b8e800584f01d1804
ddtz <ddtz32@icloud.com>
2026-04-09 22:05:56 +0800

    add lit case for lac

---
+++ demo/clang/tutorial/test/LACommenter-test.cpp
@@ -0,0 +1,27 @@
+// RUN: %clang -cc1 -load %lib/libClangTutorial.so -plugin lac %s \
+// RUN:   2>&1 | FileCheck %s
+
+// RUN: ../bin/la-commenter %s 2>&1 | FileCheck %s
+
+// CHECK-LABEL: test()
+// CHECK-NEXT: func(/*BoolArg*/true);
+// CHECK-NEXT: func(/*CharArg*/'a');
+// CHECK-NEXT: func(/*FloatArg*/1.0f);
+// CHECK-NEXT: func(/*IntArg*/1);
+// CHECK-NEXT: func(/*StringArg*/"hello");
+// CHECK-NEXT: func(Arg);
+
+void func(bool BoolArg);
+void func(char CharArg);
+void func(float FloatArg);
+void func(int IntArg);
+void func(const char *StringArg);
+
+void test(int Arg) {
+  func(true);
+  func('a');
+  func(1.0f);
+  func(1);
+  func("hello");
+  func(Arg);
+}
